109a7ecb169169c76d5abf3a75789de11ce6c60a
nessNotFound <69217845+nessaNotFound@users.noreply.github.com>
2025-04-22 10:45:19 -0400

    Initial Commit

---
+++ StateMachineDrivenAnimation/StateMachineDrivenAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateMachineDrivenAnimationApp.swift
+//  StateMachineDrivenAnimation
+//
+//  Created by Vanessa Pollice on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct StateMachineDrivenAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StateMachineDrivenAnimationTests/StateMachineDrivenAnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  StateMachineDrivenAnimationTests.swift
+//  StateMachineDrivenAnimationTests
+//
+//  Created by Vanessa Pollice on 4/22/25.
+//
+
+import Testing
+@testable import StateMachineDrivenAnimation
+
+struct StateMachineDrivenAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
